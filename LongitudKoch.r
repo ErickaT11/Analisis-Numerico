35a725ab3b3c843500b9252df28315aaef1bda1f
Ericka Torres <41376625+ErickaT11@users.noreply.github.com>
2018-10-10 07:46:58 -0500

    Update LongitudKoch.r

--- LongitudKoch.r
+++ LongitudKoch.r
@@ -1,28 +1,27 @@
- 
-#Si se considera de nuevo la primera figura, 
-#notamos que para pasar de una línea a la siguiente 
-#se remplaza tres segmentos por cuatro de igual longitud,
-#o sea que la longitud total es multiplicada por 4/3. 
-#Después de n pasos iterativos en la construcción 
-#recursiva la longitud de la curva es 3·(4/3)n, 
-#el límite de la sucesión geométrica anterior de
-#razón 4/3 es obviamente infinito, lo que significa 
-#que la figura final tiene una longitud infinita 
-#(lo que Mandelbrot denomina infinito interno). 
-#Esto está relacionado con el hecho de que la curva 
-#frontera del copo de Koch no es rectificable y tiene 
-#una dimensión fractal d > 1.
-
-
-
 CalcularLongitud <- function(iteraciones, longitudI){
-    longitud <- longitudI*((4/3)^iteraciones)
-    return(longitud)
-    }
+  longitud <- longitudI*((4/3)^(iteraciones-1))
+  return(longitud)
+}
 
-iteraciones <- 10
-
-for (i in 1:iteraciones){
-    x <- CalcularLongitud(i, 3)
-    print(x)
-    }
+distancia<-function(x1, y1, x2, y2){
+  return (sqrt ( (x2-x1)^2 + (y2-y1)^2 ) )
+}
+graficar<-function(n){
+  snowflake(n)
+  total=0
+  x=koch(side = 1, n = n)
+  for(i in 2:nrow(x)){
+    x1=x[i-1,1]
+    y1=x[i-1,2]
+    x2=x[i,1]
+    y2=x[i,2]
+    total=total+distancia (x1, y1, x2, y2)
+  }
+  x1=x[nrow(x),1]
+  y1=x[nrow(x),2]
+  x2=x[1,1]
+  y2= x[1,2]
+  total=total+distanciaPuntos (x1, y1, x2, y2)
+  cat("Longitud:", CalcularLongitud(n,3))
+}
+graficar(4)
