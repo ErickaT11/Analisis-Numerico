63c4c84da2c07eb7cc0ae097e4e74ae3cacadeb6
Ericka Torres <41376625+ErickaT11@users.noreply.github.com>
2018-10-10 07:52:25 -0500

    Update LongitudKoch.r

--- LongitudKoch.r
+++ LongitudKoch.r
@@ -3,11 +3,29 @@ CalcularLongitud <- function(iteraciones, longitudI){
   return(longitud)
 }
 
+errorPerimetro<-function (n,aprox){
+  real= 3*(4/3)^(n-1)
+  error = abs(real-aprox)
+  return (error)
+}
+
+valorTeorico<-function (n){
+  return (3*(4/3)^(n-1))
+}
+
 distancia<-function(x1, y1, x2, y2){
   return (sqrt ( (x2-x1)^2 + (y2-y1)^2 ) )
 }
+
+curvaKoch<-function(n){
+  vertices <- koch(side = 1, niter = n)
+  text <- bquote(bold(paste("Curva de Koch con n = ", .(n))))
+  plot(vertices[, 1], vertices[, 2], type = "l", asp = TRUE, main = text, xlab = "x", ylab = "y", col = "black")
+  segments(vertices[nrow(vertices), 1], vertices[nrow(vertices), 2], vertices[1, 1], vertices[1, 2], col = "black")
+}
+
 graficar<-function(n){
-  snowflake(n)
+  curvaKoch(n)
   total=0
   x=koch(side = 1, n = n)
   for(i in 2:nrow(x)){
@@ -21,7 +39,7 @@ graficar<-function(n){
   y1=x[nrow(x),2]
   x2=x[1,1]
   y2= x[1,2]
-  total=total+distanciaPuntos (x1, y1, x2, y2)
-  cat("Longitud:", CalcularLongitud(n,3))
+  total=total+distancia (x1, y1, x2, y2)
+  cat("El perimetro de la curva de Koch con n = ", n, " es: ", format(round(total,4), nsmall=4), "y el valor teorico es ",format(round(valorTeorico(n),4), nsmall=4), "(Error de",format(round(errorPerimetro(n,total),4))," )")
 }
 graficar(4)
